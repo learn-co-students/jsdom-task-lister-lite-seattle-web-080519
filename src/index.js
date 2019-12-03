cb82ae00539e72965493b6ec3a895a8282d6699c
vibraniumforge <gregm9999@gmail.com>
2019-12-03 13:36:15 -0800

    refactor in progress

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("create-task-form")
-    .addEventListener("submit", e => clickSubmit(e));
+    .addEventListener("submit", event => clickSubmit(event));
 });
 
 let counter = 1;
@@ -9,53 +9,56 @@ let notesArray = [];
 let editMode = false;
 let noteId = null;
 
-function clickSubmit(e) {
-  e.preventDefault();
-  let newTaskDescription = document.getElementById("new-task-description");
-  let priority = document.getElementById("priority-select");
-  let date = document.getElementById("date");
+const newTaskDescription = document.getElementById("new-task-description");
+const priority = document.getElementById("priority-select");
+const date = document.getElementById("date");
+const submitButton = document.getElementById("create-task-form")[3];
+
+function clickSubmit(event) {
+  event.preventDefault();
   const note = {
     id: noteId ? noteId : counter,
     text: newTaskDescription.value,
     priority: priority.value,
     date: date.value,
-    numberValue: priority.selectedIndex
+    priorityValue: priority.selectedIndex
   };
   if (noteId && editMode) {
-    notesArray.map(noteIterator => {
-      if (noteIterator.id === noteId) {
-        const x = notesArray.indexOf(noteIterator);
-        notesArray[x] = note;
-      }
-    });
+    populateNote(note);
   } else {
     notesArray.push(note);
     counter++;
   }
+  clearSubmitForm();
   loopNotes(notesArray);
-  newTaskDescription.value = "";
-  priority.value = "";
-  date.value = "";
-  editMode = false;
-  noteId = null;
+}
+
+function populateNote(note) {
+  notesArray.map(noteIterator => {
+    if (noteIterator.id === noteId) {
+      const index = notesArray.indexOf(noteIterator);
+      notesArray[index] = note;
+    }
+  });
 }
 
 function loopNotes(notesArray) {
   document.getElementById("tasks").innerHTML = "";
-  const newNotesAr = notesArray.slice();
-  newNotesAr.sort(function(a, b) {
-    return a.numberValue - b.numberValue;
+  const newNotesArray = notesArray.slice();
+  newNotesArray.sort(function(a, b) {
+    return a.priorityValue - b.priorityValue;
   });
-  newNotesAr.forEach(note => appendOneNote(note));
+  newNotesArray.forEach(note => appendOneNote(note));
 }
 
 function appendOneNote(note) {
   const tasksUl = document.getElementById("tasks");
 
   const li = document.createElement("li");
+
   const span = document.createElement("span");
   span.innerText = `${note.text} - ${note.priority} - ${note.date}`;
-  span.id = note.id;
+  span.setAttribute("id", note.id);
   span.setAttribute("class", note.priority);
 
   const deleteBtn = document.createElement("button");
@@ -83,12 +86,18 @@ function deleteNote(note) {
 
 function editNote(note) {
   editMode = true;
-  let newTaskDescription = document.getElementById("new-task-description");
-  let priority = document.getElementById("priority-select");
-  let date = document.getElementById("date");
   newTaskDescription.value = note.text;
   priority.value = note.priority;
   date.value = note.date;
   noteId = note.id;
-  editMode = true;
+  submitButton.value = "Edit A Task";
+}
+
+function clearSubmitForm() {
+  newTaskDescription.value = "";
+  priority.value = "";
+  date.value = "";
+  editMode = false;
+  noteId = null;
+  submitButton.value = "Create A Task";
 }
